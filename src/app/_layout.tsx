cdb6b033d255bf67d00c75c866d222052df23701
millan-figueroa <millan.fig@gmail.com>
2024-09-24 14:41:58 -0700

    Install missing plugins (babel-plugin-module-resolver) to make imports with @/ alias work correctly. Create tab folder to group tabs together

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,64 +1,21 @@
+import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet } from 'react-native'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 const App = () => {
-
-
 	return (
 		<SafeAreaProvider>
-			<GestureHandlerRootView style={{ flex: 1 }}>
 				<RootNavigation />
-
 				<StatusBar style="auto" />
-			</GestureHandlerRootView>
 		</SafeAreaProvider>
 	)
 }
 
 const RootNavigation = () => {
-	return (
-		<Stack>
-			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
-			<Stack.Screen
-				name="player"
-				options={{
-					presentation: 'card',
-					gestureEnabled: true,
-					gestureDirection: 'vertical',
-					animationDuration: 400,
-					headerShown: false,
-				}}
-			/>
-
-			<Stack.Screen
-				name="(modals)/addToPlaylist"
-				options={{
-					presentation: 'modal',
-					headerStyle: {
-						backgroundColor: colors.background,
-					},
-					headerTitle: 'Add to playlist',
-					headerTitleStyle: {
-						color: colors.text,
-					},
-				}}
-			/>
-		</Stack>
-
-
-	)
-
+	<Stack>
+		<Stack.screen name='(tabs)' options={{headerShown: false }} />
+	</Stack>
 
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
+export default App
