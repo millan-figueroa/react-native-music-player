ae50e3661f6572518b4aaf00dd42fdff1c8db4ba
millan-figueroa <millan.fig@gmail.com>
2024-09-25 15:42:17 -0700

    Implement custom styling to header for each category,  import StackScreenWithSearchBar and spread options we just defined in Stack.Screen for each category.

--- src/app/(tabs)/(songs)/_layout.tsx
+++ src/app/(tabs)/(songs)/_layout.tsx
@@ -1,3 +1,4 @@
+import { StackScreenWithSearchBar } from '@/constants/layout'
 import { defaultStyles } from '@/styles'
 import { Stack } from 'expo-router'
 import { View } from 'react-native'
@@ -5,11 +6,11 @@ import { View } from 'react-native'
 const SongsScreenLayout = () => {
 	return (
 		<View style={defaultStyles.container}>
-			<Stack>
+		<Stack>
 				<Stack.Screen
 					name="index"
 					options={{
-						
+						...StackScreenWithSearchBar,
 						headerTitle: 'Songs',
 					}}
 				/>
