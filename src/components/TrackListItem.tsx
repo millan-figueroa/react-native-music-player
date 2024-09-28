73a0a8b4a06697188b62d97142d8bd0047565043
millan-figueroa <millan.fig@gmail.com>
2024-09-27 17:26:16 -0700

    Rename TrackListItemProps and TrackListItem.tsx, update imports, added styles for trackArtistText.

--- src/components/TrackListItem.tsx
+++ src/components/TrackListItem.tsx
@@ -1,41 +1,50 @@
+import { unknownTrackImageUri } from '@/constants/images';
+import { colors, fontSize } from '@/constants/tokens';
+import { defaultStyles } from '@/styles';
 import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import FastImage from 'react-native-fast-image';
 
-export type TracklistItemProps = {
+export type TrackListItemProps = {
     track: { title: string; image?: string, artist?: string }
 }
 
-export const TrackListItem = ({ track }: TracklistItemProps) => {
+export const TrackListItem = ({ track }: TrackListItemProps) => {
     const isActiveTrack = false
 
     return (
         <TouchableHighlight>
-            <View>
-                <FastImage
-                    source={{
-                                uri: track.image ?? unknownTrackImageUri,
-                                priority: FastImage.priority.normal,
-                            }}
-                    style={{
-                                ...styles.trackArtworkImage,
-                                opacity: isActiveTrack ? 0.6 : 1,
-                            }}
-                />
-            </View>
+            <View style={styles.trackItemContainer}>
+                <View>
+                    <FastImage
+                        source={{
+                                    uri: track.image ?? unknownTrackImageUri,
+                                    priority: FastImage.priority.normal,
+                                }}
+                        style={{
+                                    ...styles.trackArtworkImage,
+                                    opacity: isActiveTrack ? 0.6 : 1,
+                                }}
+                    />
+                </View>
 
-            {/* Track title + artist */}
-            <View style={{ width: '100%' }}>
-                <Text 
-                    numberOfLines={1}
-                    style={{
-                        ...styles.trackTitleText,
-                        color: isActiveTrack ? colors.primary : colors.text,
-                    }}
-                >
+                {/* Track title + artist */}
+                <View style={{ width: '100%' }}>
+                    <Text 
+                        numberOfLines={1}
+                        style={{
+                            ...styles.trackTitleText,
+                            color: isActiveTrack ? colors.primary : colors.text,
+                        }}
+                    >
                     {track.title}
-                </Text>
-            </View>     
-
+                    </Text>
+                    {track.artist && (
+                                <Text numberOfLines={1} style={styles.trackArtistText}>
+                                    {track.artist}
+                                </Text>
+                            )}
+                </View>     
+            </View>
         </TouchableHighlight>
     )
 }
@@ -53,5 +62,12 @@ const styles = StyleSheet.create({
 		fontSize: fontSize.sm,
 		fontWeight: '600',
 		maxWidth: '90%',
+	},
+    trackArtistText: {
+		...defaultStyles.text,
+		color: colors.textMuted,
+		fontSize: 14,
+		marginTop: 4,
 	}
+
 })
